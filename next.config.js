663f4dec55e7330c1d6801177eeb15685f38d74a
ZeusAIGod <zeusai909@gmail.com>
2025-01-19 12:01:00 +0900

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+const nextConfig = {
+  /* config options here */
+};
+
+// export default nextConfig;
+// export default nextConfig;
